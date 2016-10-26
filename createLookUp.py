a06885dedd834d69db7b0b2218718782ace888fc
Jeff Kimbrel <jakpot@gmail.com>
2016-10-26 15:58:56 -0700

    Look Up Table added
    createLookUp.py to make a tab-delimited .txt file of fields in a gen
    bank file. This is useful, for example, to get a list of protein_ids
    and their products.

---
+++ createLookUp.py
@@ -0,0 +1,59 @@
+from Bio import SeqIO
+import re
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Give it several fields, and it will create a tab-delimited lookup tab-delimited lookup table')
+
+parser.add_argument('-g', '--genbank',
+    help="Genbank file to summarize",
+    required=True)
+
+parser.add_argument('-p', '--primary',
+    default="protein_id",
+    help="Primary qualifier" )
+
+parser.add_argument('-s', '--secondary',
+    default="gene_synonym,gene",
+    help="Secondary qualifier(s)" )
+
+parser.add_argument('-f', '--filter',
+    default="",
+    help="Use only secondary qualifiers that contain this field" )
+
+args = parser.parse_args()
+
+qualifiers = {}
+
+# go through once to get all of the primary qualifiers, add to a dictionary of lists
+for seq_record in SeqIO.parse(args.genbank, "genbank"):
+    for feature in seq_record.features:
+        for qualifierKey in feature.qualifiers:
+            if qualifierKey == args.primary:
+                for primaryID in feature.qualifiers[qualifierKey]:
+                    qualifiers[primaryID] = []
+
+#
+for seq_record in SeqIO.parse(args.genbank, "genbank"):
+    for feature in seq_record.features:
+        primaryList = ""
+        secondaryList = []
+        for qualifierKey in feature.qualifiers:
+            if qualifierKey == args.primary:
+                primaryList = feature.qualifiers[qualifierKey]
+
+            split = args.secondary.split(",")
+            for secondary in split:
+                if qualifierKey == secondary:
+                    for secondSecondary in feature.qualifiers[qualifierKey]:
+                        secondaryList.append(secondSecondary)
+
+        for primary in primaryList:
+            for secondary in secondaryList:
+                qualifiers[primary].append(secondary)
+
+#
+for primary in qualifiers:
+    for secondary in qualifiers[primary]:
+        if args.filter in secondary:
+            print(primary,secondary,sep="\t")
--- mergeGenbank.py
+++ mergeGenbank.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(description='Merge CDS qualifiers of a secondar
 parser.add_argument('-m', '--master',
     help="Master genbank file",
     required=True)
-    
+
 parser.add_argument('-s', '--secondary',
     help="Genbank file to merge into master",
     required=True)
@@ -21,8 +21,7 @@ parser.add_argument('-s', '--secondary',
 parser.add_argument('-f', '--folder',
     help="Output folder, defaults to mergedOutput + timestamp",
     default="mergedOutput_"+timestamp)
-    
-    
+
 args = parser.parse_args()
 
 masterGenbankFH = args.master
@@ -34,48 +33,48 @@ print("\nOUTPUT FOLDER: "+outputFolder+"\n")
 if not os.path.exists(outputFolder):
     os.makedirs(outputFolder)
 
-## 
+##
 for masterGenbank in SeqIO.parse(masterGenbankFH, "genbank"):
     masterLength = len(masterGenbank.seq)
     print("ID: "+masterGenbank.id+"\nLENGTH: "+str(masterLength))
 
     secondaryQualifiers = {}
-    
+
     ## SAVE SECONDARY FEATURES/QUALIFIERS FROM SOURCES OF SAME LENGTH
     for secondaryGenbank in SeqIO.parse(secondaryGenbankFH, "genbank"):
         secondaryLength = len(secondaryGenbank.seq)
 
         if secondaryLength == masterLength:
-            
+
             ## loop through features
             for secondaryRecord in secondaryGenbank.features:
                 if secondaryRecord.type == 'CDS':
                     secondaryQualifiers[str(secondaryRecord.location)] = secondaryRecord.qualifiers
-        
+
     ## NOW GO THROUGH MASTER FEATURES, AND ADD SECONDARY QUALIFIERS
     for masterRecord in masterGenbank.features:
         if masterRecord.type == 'CDS':
             if str(masterRecord.location) in secondaryQualifiers:
-                                
+
                 ## COMBINE TWO DICTIONARIES
                 combinedQualifiers = masterRecord.qualifiers
                 for key in secondaryQualifiers[str(masterRecord.location)]:
-                    
+
                     if key in combinedQualifiers:
                         for qualifier in secondaryQualifiers[str(masterRecord.location)][key]:
-                            combinedQualifiers[key].append(qualifier) 
+                            combinedQualifiers[key].append(qualifier)
                     else:
                         combinedQualifiers[key] = secondaryQualifiers[str(masterRecord.location)][key]
-                        
-                
-                ## REMOVE QUALIFIER REDUNDANCY        
+
+
+                ## REMOVE QUALIFIER REDUNDANCY
                 for qualifier in combinedQualifiers:
                     combinedQualifiers[qualifier] = list(set(combinedQualifiers[qualifier]))
-                
-                masterRecord.qualifiers = combinedQualifiers 
-                
-                
-                
+
+                masterRecord.qualifiers = combinedQualifiers
+
+
+
     ### Write to file
     output_handle = open(outputFolder+"/"+masterGenbank.name+"_merged.gbk", "w")
     SeqIO.write(masterGenbank, output_handle, "genbank")
