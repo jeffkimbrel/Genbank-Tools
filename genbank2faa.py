850a91b263078654d4bfcb7cfcc93fce1966fbf0
Jeff Kimbrel <jakpot@gmail.com>
2017-08-22 18:41:59 -0700

    Updates

--- genbank2faa.py
+++ genbank2faa.py
@@ -20,6 +20,6 @@ for seq_record in SeqIO.parse(args.genbank, "genbank"):
     for feature in seq_record.features:
         if 'translation' in feature.qualifiers:
             if feature.qualifiers[args.identifier][0] == "":
-                print("ERROR: NO HEADER FOUND: ",feature.qualifiers['translation'][0],file=sys.stderr)
+                print("ERROR: NO HEADER FOUND: ", feature.qualifiers['translation'][0], file = sys.stderr)
             else:
                 print(">"+str(feature.qualifiers[args.identifier][0])+"\n"+str(feature.qualifiers['translation'][0]))
