c361c77c2c850ddde22b2844037ffedbe73d4293
Jeff Kimbrel <jakpot@gmail.com>
2018-01-12 11:37:27 -0800

    added incrementVersion() function
    Increments the id.x version number of a genbank file

--- functions.py
+++ functions.py
@@ -29,3 +29,13 @@ def cleanEC(ecList):
     ecListFinal = incompleteList + completeList
 
     return(ecListFinal)
+
+def incrementVersion(seq_record):
+    if "sequence_version" in seq_record.annotations:
+        version = seq_record.annotations["sequence_version"] + 1
+        seq_record.id = str(seq_record.name) + "." + str(version)
+
+    else:
+        seq_record.id = str(seq_record.name) + "." + str(1)
+
+    return(seq_record)
