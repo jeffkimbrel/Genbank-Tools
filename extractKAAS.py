c5fcea1c15bb959db49063a65bc85af6f7f1d2c3
Jeff Kimbrel <jakpot@gmail.com>
2018-05-17 13:13:49 -0700

    Works with KEGG API files

--- extractKAAS.py
+++ extractKAAS.py
@@ -20,30 +20,32 @@ args = parser.parse_args()
 
 koMap = {}
 
-koFile = os.path.dirname(sys.argv[0]) + "/tools/keggNameDefs.txt"
+koFile = os.path.dirname(sys.argv[0]) + "/tools/koOrthology.txt"
 for line in open(koFile, 'rt'):
     line = line.rstrip()
 
     split = line.split("\t")
-    ko = split[0]
+    ko = split[0].split(":")[1]
     product = split[1]
 
     koMap[ko] = {'product' : product}
 
-ecFile = os.path.dirname(sys.argv[0]) + "/tools/kegg2EC.txt"
+ecFile = os.path.dirname(sys.argv[0]) + "/tools/koEC.txt"
 for line in open(ecFile, 'rt'):
     line = line.rstrip()
 
     split = line.split("\t")
-    ko = split[0]
-    product = split[2].split(" [")[0]
-    ec = tools.anno.cleanEC(split[3:])
+    ko = split[0].split(":")[1]
+    ec = split[1].split(":")[1]
 
     if ko in koMap:
-        koMap[ko]['EC_number'] = ec
+        if 'ec' in koMap[ko]:
+            koMap[ko]['ec'].append(ec)
+        else:
+            koMap[ko]['ec'] = [ec]
+
     else:
-        koMap[ko] = {'product' : product,
-                     'EC_number' : ec}
+        koMap[ko] = {'ec' : [ec]}
 
 ## PROCESS KEGG FILE ###########################################################
 
@@ -61,6 +63,6 @@ for line in lines:
                 print(gene, 'product', koMap[ko]['product'], sep = "\t")
                 print(gene, 'db_xref', "KO:"+str(ko), sep = "\t")
 
-                if 'EC_number' in koMap[ko]:
-                    for ec in koMap[ko]['EC_number']:
-                        print(gene, 'EC_number', ec, sep = "\t")
+                if 'ec' in koMap[ko]:
+                    for ec in koMap[ko]['ec']:
+                        print(gene, 'ec', ec, sep = "\t")
